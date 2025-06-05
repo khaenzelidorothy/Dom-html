791d91e86d015072b36b6ec58c80fb4baaf09eaa
khaenzelidorothy <khaenzelidorothy@gmail.com>
2025-06-05 18:49:29 +0300

    Aligned different elements

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ body.style.fontSize ='0.2in'
 const title =document.getElementById('title');
 title.style.color ='green'
 title.style.fontSize ='0.4in'
-title.style.marginTop ='20%'
+title.style.marginTop ='18%'
 title.style.marginLeft ='11%'
 // heading.style.color ='#004526'
 
@@ -75,6 +75,11 @@ generateLi.forEach((li,index) =>{
     const img = document.createElement('img');
     img.src = images[index];
     img.classList.add('list-img');
+
+     if (index === 2) {
+    img.style.width = '67%';
+    img.style.height = '67%';
+  }
      img.style.marginRight = '10px';
     img.style.verticalAlign = 'middle';
     img.style.borderRadius = '10%';
@@ -87,13 +92,13 @@ const navList = document.querySelectorAll('a');
 navList.forEach((list) =>{
   list.style.textDecoration ='none'
   list.style.color ='green'
-  list.style.marginRight='3%'
+  list.style.marginRight='12%'
 })
 
 const navigation = document.getElementById('navigation')
-navigation.style.marginLeft ='72%'
-navigation.style.marginTop ='-2.5%'
-navigation.style.fontSize ='0.18in'
+navigation.style.marginLeft ='52%'
+navigation.style.marginTop ='-2%'
+navigation.style.fontSize ='0.2in'
 // navigation.style.gap='2px'
 
 const image =document.createElement('img')
@@ -106,9 +111,21 @@ anImage.appendChild(image)
 
 const logo =document.getElementById('logo')
 logo.style.marginLeft ='7%'
+logo.style.paddingTop ='2%'
 
 const paragraph =document.getElementById('paragraph')
 paragraph.style.marginLeft ='11%'
 
+const button = document.getElementById('button')
+const aButton =document.createElement('button')
+aButton.textContent = 'order now'
+button.appendChild(aButton)
+aButton.style.padding = '15px 29px';          
+aButton.style.fontSize = '1rem';            
+aButton.style.marginLeft= '19%';               
+aButton.style.border = 'none';                
+aButton.style.borderRadius = '18px';          
+aButton.style.cursor = 'pointer';           
+aButton.style.boxShadow = '0 2px 8px #ccc';   
 
 
